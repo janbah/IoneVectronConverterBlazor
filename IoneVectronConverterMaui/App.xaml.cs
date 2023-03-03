18c3cd29f434fb3d510f18a775dda5cf0af7419f
Jan Bahlmann <jbh@ione-software.com>
2023-03-03 20:39:35 +0100

    add maui project

---
+++ IoneVectronConverterMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace IoneVectronConverterMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ IoneVectronConverterMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace IoneVectronConverterMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
