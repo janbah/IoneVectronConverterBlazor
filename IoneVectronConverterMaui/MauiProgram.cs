b233c394f606611a4bc759b78feb5dedae3c9841
Jan Bahlmann <jbh@ione-software.com>
2023-03-08 14:45:59 +0100

    fix dependency injection

--- IoneVectronConverterMaui/MauiProgram.cs
+++ IoneVectronConverterMaui/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using ConnectorLib.Common.Datastoring;
+﻿
+using ConnectorLib.Common.Datastoring;
 using ConnectorLib.Ione.Categories;
 using ConnectorLib.Ione.Client;
 using ConnectorLib.Vectron.Client;
@@ -25,30 +26,26 @@ namespace IoneVectronConverterMaui
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
-
-
             builder.Services.AddMudServices();
-
+            
             var path = @"C:\Users\JanBahlmann\source\IoneVectronConverterBlazor\IoneVectronConverterMaui";
             
             var config = new ConfigurationBuilder()
                 .SetBasePath(path)
                 .AddJsonFile("appsettings.json")
                 .Build();
-
             builder.Configuration.AddConfiguration(config);
+
+            builder.Services.AddTransient<IVectronClient, VectronClient>();
             
             builder.Services.AddTransient<IIoneClient, IoneClient>();
-
+            
             builder.Services.AddTransient<CategoryMapper>();
 
             builder.Services.AddTransient<ICategoryManager, CategoryManager>();
-
+            
             builder.Services.AddTransient<IRepository<Category>, CategoryRepository>();
             builder.Services.AddTransient<ICategoryService, CategoryService>();
-
-
-            builder.Services.AddTransient<IVectronClient, VectronClient>();
 
             builder.Services.AddTransient<IRepository<PLU>, PluRepository>();
             builder.Services.AddTransient<IPluService, PluService>();
@@ -62,6 +59,12 @@ namespace IoneVectronConverterMaui
             builder.Services.AddTransient<IDepartmentService, DepartmentService>();
 
             builder.Services.AddTransient<IMasterdataReceiver, MasterdataReceiver>();
+            builder.Services.AddMauiBlazorWebView();
+
+     
+
+
+            
 
             
 #if DEBUG
@@ -69,6 +72,7 @@ namespace IoneVectronConverterMaui
 		    //builder.Logging.AddDebug();
 #endif
 
+            builder.Services.AddSingleton<WeatherForecastService>();
 
             return builder.Build();
         }
